56ecffe013c5cac42609d0877e0c96e4d96b8af0
los Merengue <michaels4uandme@gmail.com>
2022-04-23 23:44:24 +0100

    Add comment to the provider segment of the code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws"{
     region = "us-east-1"
-    access_key = "AKIA2WNT522GFDZTBJIN"
-    secret_key = "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"
+    access_key = #"AKIA2WNT522GFDZTBJIN"  This security key is outdated
+    secret_key = #"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" 
 }
 
 # Create a variable for IP address
